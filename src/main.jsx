0666d4ebacb583e564ea436010b80a3c8b081680
mahdi golamozadeh <mahdi.paydar.smart@gmail.com>
2023-09-22 12:25:40 +0330

    deploy website

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { MyContextProvider } from "./components/MyContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
     <MyContextProvider>
       <App />
     </MyContextProvider>
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
