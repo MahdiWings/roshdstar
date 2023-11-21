31abec8bd4f9e6c3c21242c01ff913c0e6117b24
mahdi golamozadeh <mahdi.paydar.smart@gmail.com>
2023-11-21 19:33:14 +0330

    update host 0.0.0.0 and vite

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  target: 'server',
-  server: {
-    host: '0.0.0.0',
-  },
   build: {
     outDir: "dist", // مکان فایل‌های ساخته شده
   },
