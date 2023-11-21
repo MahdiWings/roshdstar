edccd3dff514931a37b43bdb84300a7ffbc595c2
mahdi golamozadeh <mahdi.paydar.smart@gmail.com>
2023-11-21 11:57:05 +0330

    change src root

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+// import App from "./App.jsx";
+import App from "../src/App";
 import { MyContextProvider } from "./components/MyContext";
 import "./index.css";
 
